acb1164019d129285267b79844d45ac5f75f669c
Pascal Lohscheidt <pascal.lohscheidt@gmail.com>
2019-08-04 18:49:59 +0200

    added new axios modules

--- mind-proof/src/components/post_creation_page/PostCreationPage.js
+++ mind-proof/src/components/post_creation_page/PostCreationPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import TitleTextField from "./TitleTextField";
 import SummaryTextField from "./SummaryTextField";
 import ArticleTextField from "./ArticleTextField";
@@ -6,12 +8,17 @@ import ArticleTextField from "./ArticleTextField";
 
 class PostCreationPage extends React.Component
 {
-    state = {
-        previewOn: false,
-        author: 'test'
+    constructor(){
+        super();
+        this.state = {
+            title: '',
+            article: ''
+        }
     }
 
     render(){
+        // axios.get('http://127.0.0.1:8000/api/v1/posts/?format=json').then(response => this.setState({title: response.data.title, article: response.data.article}));
+        axios.get('http://127.0.0.1:8000/api/v1/posts/?format=json').then(response => console.log(response));
         return(
             <div id={ 'post-creation-page' }>
                 <TitleTextField />
