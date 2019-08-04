c80f7ef739d6d78008885eccfb5653f200d81821
Pascal Lohscheidt <pascal.lohscheidt@gmail.com>
2019-08-04 16:26:55 +0200

    added some components such as post-creation page

---
+++ mind-proof/src/components/post_creation_page/PostCreationPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import TitleTextField from "TitleTextField";
+
+state = {
+    previewOn: false
+}
+
+
+const PostCreationPage = () => {
+    return(
+        <div>
+            <TitleTextField />
+            
+        </div>
+    );
+}
+
+
+
+export default PostCreationPage;
---
+++ mind-proof/src/components/post_creation_page/TitleTextField.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TitleTextField = () => {
+    return(
+        <div>
+            <input id={ 'post-creation-title' } type={"text"} placeholder={ 'title' }/>
+        </div>
+    );
+}
+
+export default TitleTextField;
