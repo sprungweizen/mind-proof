3c8e46ea40659506ddbc05dba7c1d723b61bd9c3
Moritz Grundei <grundeires@gmail.com>
2019-08-04 17:42:40 +0200

    added virtualenv, wasnt staged before

---
+++ back-end/virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/af41b064-6712-4c8c-b8dd-420803879798/project_Mindp/mind-proof/back-end/virtualenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
