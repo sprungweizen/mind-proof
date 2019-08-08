ca97014fd4f16e9f8876bf5b14a3be40ab519057
Moritz Grundei <grundeires@gmail.com>
2019-08-08 22:05:56 +0200

    update RESTAPI url (remove path) and added test_backend which fails as expected

--- back-end/mindProof/posts/tests/test_api.py
+++ back-end/mindProof/posts/tests/test_api.py
@@ -4,5 +4,5 @@ from rest_framework.test import RequestsClient
 class TestAPIGet(TestCase):
     def test_client_gets_200(self):
         client= RequestsClient()
-        response = client.get("http://127.0.0.1:8000/api/v1/posts/")
+        response = client.get("http://localhost:8000/api/posts/")
         self.assertEqual(response.status_code, 200)
---
+++ back-end/mindProof/posts/tests/test_backend.py
@@ -0,0 +1,37 @@
+'''this test file intends to test the backend as an isolated object and confront it with sometimes unusual -but never impossible-
+ situations. Technically, this test doesnt belong inside the posts directory/app as it is not a unit test anymore'''
+
+from django.test import TestCase
+#this import is ugly and needs to be replaced once we introduce CD mechanisms as it will only work in deployment
+from ..models import Post
+import time
+import multiprocessing
+
+#helper methods
+def get_dummy_post(title):
+    post = Post()
+    post.title = title #pk and identifier, hence hardcoded
+    #as Image Field is nullable, this field will be null by default
+    post.article = "Hello I am a dummy post"
+    post.date_added = time.time()
+    post.save()
+
+#test whether backend (including the database layer) can handle more than one request at the same time without throwing an error
+class TestLoadManagement(TestCase):
+
+    def test_can_process_two_requests_at_the_same_time(self):
+        def process1():
+            get_dummy_post("the first post")
+        def process2():
+            get_dummy_post("the second post")
+        p1 = multiprocessing.Process(target=process1)
+        p2 = multiprocessing.Process(target=process2)
+        #should not raise
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+        self.assertEqual(2,len(Post.objects.all()))
+        print("[TEST_INFO]: Test test_can_handle_two_requests_at_the_same_time_passed")
+
+
